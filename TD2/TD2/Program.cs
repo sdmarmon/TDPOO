95b4e80f19430e81f27186afe928f40f1ebf1ce3
Sylvain Duhau-marmon <sdmarmon@ensc.fr>
2017-09-26 13:53:19 +0200

    Exercice 3 fait Signed-off-by: Sylvain Duhau-marmon <sdmarmon@ensc.fr>

---
+++ TD2/TD2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TD2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IZoomable> l1 = new List<IZoomable>();
+            Cube c1 = new Cube(1);
+            Parallelepipede p1 = new Parallelepipede(9, 5, 7);
+            l1.Add(c1);
+            l1.Add(p1);
+            TestZoomable l2 = new TestZoomable(l1, 10);
+            Console.ReadLine();
+        }
+    }
+}
