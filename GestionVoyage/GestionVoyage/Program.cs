01b79d82a612a861ac51055ce085d4d00565e7d5
Sylvain Duhau-marmon <sdmarmon@ensc.fr>
2017-09-27 11:46:16 +0200

    Gestion Voyage Signed-off-by: Sylvain Duhau-marmon <sdmarmon@ensc.fr>

---
+++ GestionVoyage/GestionVoyage/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GestionVoyage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Simulateur s = new Simulateur(5, "s", 5000, 130);
+        }
+    }
+}
